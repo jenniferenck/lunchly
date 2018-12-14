d02de072da80fad0f1ca005734a4eec869b0df62
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2018-12-14 12:36:40 -0800

    starter code

---
+++ route-helpers.js
@@ -0,0 +1,19 @@
+const Customer = require('./models/customer');
+
+async function addOrEditCustomer(req, res, next) {
+  try {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const phone = req.body.phone;
+    const notes = req.body.notes;
+
+    const customer = new Customer({ firstName, lastName, phone, notes });
+    await customer.save();
+
+    return res.redirect(`/${customer.id}/`);
+  } catch (err) {
+    return next(err);
+  }
+}
+
+module.exports = addOrEditCustomer;
---
+++ routes.js
@@ -0,0 +1,103 @@
+/** Routes for Lunchly */
+
+const express = require('express');
+
+const Customer = require('./models/customer');
+const Reservation = require('./models/reservation');
+const addEditCustomer = require('./route-helpers');
+
+const router = new express.Router();
+
+/** Homepage: show list of customers. */
+
+router.get('/', async function(req, res, next) {
+  try {
+    const customers = await Customer.all();
+    return res.render('customer_list.html', { customers });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** Form to add a new customer. */
+
+router.get('/add/', async function(req, res, next) {
+  try {
+    return res.render('customer_new_form.html');
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** Handle adding a new customer. */
+
+router.post('/add/', async function(req, res, next) {
+  return await addEditCustomer(req, res, next);
+});
+
+/** Show a customer, given their ID. */
+
+router.get('/:id/', async function(req, res, next) {
+  try {
+    req.customer = await Customer.get(req.params.id);
+
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.get('/:id/', async function(req, res, next) {
+  try {
+    const customer = req.customer;
+
+    const reservations = await customer.getReservations();
+
+    return res.render('customer_detail.html', { customer, reservations });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** Show form to edit a customer. */
+
+router.get('/:id/edit/', async function(req, res, next) {
+  try {
+    const customer = req.customer;
+
+    res.render('customer_edit_form.html', { customer });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** Handle editing a customer. */
+
+router.post('/:id/edit/', async function(req, res, next) {
+  return await addEditCustomer(req, res, next);
+});
+
+/** Handle adding a new reservation. */
+
+router.post('/:id/add-reservation/', async function(req, res, next) {
+  try {
+    const customerId = req.params.id;
+    const startAt = new Date(req.body.startAt);
+    const numGuests = req.body.numGuests;
+    const notes = req.body.notes;
+
+    const reservation = new Reservation({
+      customerId,
+      startAt,
+      numGuests,
+      notes
+    });
+    await reservation.save();
+
+    return res.redirect(`/${customerId}/`);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
